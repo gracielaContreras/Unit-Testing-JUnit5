9f8a02543d083db5316303c48fdf899277a0489a
Graciela Contreras <gracielaestefani@gmail.com>
2023-01-20 01:02:26 -0300

    Ejercicio de codewars.com String ends with?

---
+++ src/test/java/org/gcontreras/junit5app/ejemplos/models/StringEndsWitch.java
@@ -0,0 +1,31 @@
+package org.gcontreras.junit5app.ejemplos.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringEndsWitch {
+    @Test
+    public void staticTests() {
+        check("samurai", "ai", true);
+        check("sumo", "omo", false);
+        check("ninja", "ja", true);
+        check("sensei", "i", true);
+        check("samurai", "ra", false);
+        check("abc", "abcd", false);
+        check("abc", "abc", true);
+        check("abcabc", "bc", true);
+        check("ails", "fails", false);
+        check("fails", "ails", true);
+        check("this", "fails", false);
+        check("this", "", true);
+        check(":-)", ":-(", false);
+        check("!@#$%^&*() :-)", ":-)", true);
+        check("abc\n", "abc", false);
+    }
+
+    private static void check(String str, String ending, boolean expected) {
+        boolean result = StringEndsWith.solution(str, ending);
+        assertEquals(expected, result, "Expected solution(\"" + str + "\", \"" + ending + "\") to return " + expected);
+    }
+}
